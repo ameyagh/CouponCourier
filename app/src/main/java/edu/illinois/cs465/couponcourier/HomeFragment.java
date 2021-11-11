a7b2a994ad215af80fe4f3b5a70399aa0f057d91
Nick Bellizzi <ngb3@illinois.edu>
2021-11-10 20:19:42 -0600

    Drafted home screen

--- app/src/main/java/edu/illinois/cs465/couponcourier/HomeFragment.java
+++ app/src/main/java/edu/illinois/cs465/couponcourier/HomeFragment.java
@@ -1,12 +1,13 @@
 package edu.illinois.cs465.couponcourier;
 
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.Fragment;
+
 
 /**
  * A simple {@link Fragment} subclass.
@@ -58,7 +59,12 @@ public class HomeFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+        SearchView searchView = view.findViewById(R.id.search_bar);
+        searchView.setQueryHint("search by brand, category, coupon type, etc.");
+        // Makes entire bar clickable
+        searchView.setIconified(false);
+        searchView.clearFocus();
+        return view;
     }
 }
